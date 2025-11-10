8544fc7da682bee7d342f748e81815fad339b1bb
Dr Lembke <lembke@msoe.edu>
2025-11-10 14:42:11 -0600

    add pages to tracer

--- gp-workloads/mixed.sh
+++ gp-workloads/mixed.sh
@@ -7,7 +7,7 @@ echo "Starting mixed workload tests..."
 echo "Running system call tracer..."
 sudo killall python3
 sudo rm -f out/mixed*.csv
-sudo python3 tracer_rt.py -o out/mixed.csv -b 1024 &
+sudo python3 tracer_rt.py -o out/mixed.csv -b 2048 &
 TRACER_PID=$!
 sleep 15
 
