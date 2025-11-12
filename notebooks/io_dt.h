0994ca0393916f0c6f89d6676379032fc984448e
BradenEverson <bradeneverson@gmail.com>
2025-11-12 10:09:48 -0600

    Train multi-class models for IO and CPU intensivity, quantize/export them

---
+++ notebooks/io_dt.h
@@ -0,0 +1,390 @@
+
+
+
+    // !!! This file is generated using emlearn !!!
+
+    #include <stdint.h>
+    
+
+static inline int32_t io_dt_tree_0(const int16_t *features, int32_t features_length) {
+          if (features[3] < 39) {
+              if (features[0] < 5252) {
+                  if (features[3] < 6) {
+                      return 0;
+                  } else {
+                      if (features[2] < 1986) {
+                          if (features[3] < 17) {
+                              return 1;
+                          } else {
+                              if (features[1] < 97) {
+                                  return 2;
+                              } else {
+                                  return 1;
+                              }
+                          }
+                      } else {
+                          return 1;
+                      }
+                  }
+              } else {
+                  if (features[0] < 5752) {
+                      if (features[3] < 21) {
+                          return 0;
+                      } else {
+                          return 1;
+                      }
+                  } else {
+                      return 0;
+                  }
+              }
+          } else {
+              if (features[3] < 26720) {
+                  if (features[3] < 1869) {
+                      if (features[3] < 281) {
+                          if (features[2] < 13492) {
+                              if (features[3] < 119) {
+                                  if (features[2] < 5136) {
+                                      if (features[1] < 185) {
+                                          return 2;
+                                      } else {
+                                          return 1;
+                                      }
+                                  } else {
+                                      if (features[3] < 107) {
+                                          return 1;
+                                      } else {
+                                          return 1;
+                                      }
+                                  }
+                              } else {
+                                  if (features[1] < 367) {
+                                      if (features[0] < 2752) {
+                                          return 1;
+                                      } else {
+                                          return 2;
+                                      }
+                                  } else {
+                                      if (features[0] < 7502) {
+                                          return 2;
+                                      } else {
+                                          return 1;
+                                      }
+                                  }
+                              }
+                          } else {
+                              if (features[3] < 81) {
+                                  return 0;
+                              } else {
+                                  if (features[3] < 245) {
+                                      if (features[1] < 10) {
+                                          return 1;
+                                      } else {
+                                          return 1;
+                                      }
+                                  } else {
+                                      if (features[2] < 18176) {
+                                          return 2;
+                                      } else {
+                                          return 1;
+                                      }
+                                  }
+                              }
+                          }
+                      } else {
+                          if (features[0] < 9252) {
+                              if (features[1] < 4251) {
+                                  if (features[2] < 7872) {
+                                      if (features[3] < 291) {
+                                          return 2;
+                                      } else {
+                                          return 1;
+                                      }
+                                  } else {
+                                      return 2;
+                                  }
+                              } else {
+                                  return 2;
+                              }
+                          } else {
+                              if (features[0] < 29752) {
+                                  if (features[3] < 1620) {
+                                      if (features[3] < 494) {
+                                          return 2;
+                                      } else {
+                                          return 2;
+                                      }
+                                  } else {
+                                      return 1;
+                                  }
+                              } else {
+                                  if (features[3] < 509) {
+                                      if (features[0] < 30752) {
+                                          return 1;
+                                      } else {
+                                          return 1;
+                                      }
+                                  } else {
+                                      if (features[3] < 764) {
+                                          return 2;
+                                      } else {
+                                          return 2;
+                                      }
+                                  }
+                              }
+                          }
+                      }
+                  } else {
+                      if (features[0] < 3252) {
+                          if (features[2] < 358) {
+                              return 1;
+                          } else {
+                              if (features[3] < 24001) {
+                                  return 2;
+                              } else {
+                                  return 1;
+                              }
+                          }
+                      } else {
+                          if (features[1] < 158) {
+                              return 2;
+                          } else {
+                              if (features[1] < 55524) {
+                                  if (features[2] < 50179) {
+                                      return 1;
+                                  } else {
+                                      if (features[0] < 54502) {
+                                          return 2;
+                                      } else {
+                                          return 1;
+                                      }
+                                  }
+                              } else {
+                                  return 2;
+                              }
+                          }
+                      }
+                  }
+              } else {
+                  if (features[1] < 8659) {
+                      if (features[3] < 163255) {
+                          if (features[1] < 1614) {
+                              if (features[3] < 147812) {
+                                  if (features[2] < 6) {
+                                      return 2;
+                                  } else {
+                                      if (features[1] < 1452) {
+                                          return 1;
+                                      } else {
+                                          return 1;
+                                      }
+                                  }
+                              } else {
+                                  return 2;
+                              }
+                          } else {
+                              if (features[3] < 125149) {
+                                  if (features[0] < 3252) {
+                                      if (features[3] < 57403) {
+                                          return 2;
+                                      } else {
+                                          return 1;
+                                      }
+                                  } else {
+                                      if (features[3] < 109779) {
+                                          return 2;
+                                      } else {
+                                          return 2;
+                                      }
+                                  }
+                              } else {
+                                  if (features[1] < 1959) {
+                                      if (features[1] < 1687) {
+                                          return 2;
+                                      } else {
+                                          return 1;
+                                      }
+                                  } else {
+                                      if (features[2] < 2257) {
+                                          return 2;
+                                      } else {
+                                          return 2;
+                                      }
+                                  }
+                              }
+                          }
+                      } else {
+                          if (features[1] < 2670) {
+                              if (features[1] < 2299) {
+                                  if (features[1] < 1460) {
+                                      return 2;
+                                  } else {
+                                      if (features[2] < 10822) {
+                                          return 1;
+                                      } else {
+                                          return 2;
+                                      }
+                                  }
+                              } else {
+                                  if (features[1] < 2542) {
+                                      return 2;
+                                  } else {
+                                      if (features[1] < 2588) {
+                                          return 1;
+                                      } else {
+                                          return 2;
+                                      }
+                                  }
+                              }
+                          } else {
+                              if (features[2] < 22629) {
+                                  if (features[3] < 506339) {
+                                      if (features[2] < 800) {
+                                          return 1;
+                                      } else {
+                                          return 1;
+                                      }
+                                  } else {
+                                      if (features[1] < 6012) {
+                                          return 1;
+                                      } else {
+                                          return 1;
+                                      }
+                                  }
+                              } else {
+                                  if (features[3] < 986782) {
+                                      if (features[2] < 29097) {
+                                          return 2;
+                                      } else {
+                                          return 2;
+                                      }
+                                  } else {
+                                      return 1;
+                                  }
+                              }
+                          }
+                      }
+                  } else {
+                      if (features[3] < 382931) {
+                          if (features[3] < 303399) {
+                              if (features[1] < 9781) {
+                                  if (features[1] < 9732) {
+                                      if (features[3] < 203108) {
+                                          return 2;
+                                      } else {
+                                          return 2;
+                                      }
+                                  } else {
+                                      return 1;
+                                  }
+                              } else {
+                                  return 2;
+                              }
+                          } else {
+                              if (features[1] < 13312) {
+                                  if (features[2] < 2432) {
+                                      if (features[3] < 316014) {
+                                          return 1;
+                                      } else {
+                                          return 2;
+                                      }
+                                  } else {
+                                      return 1;
+                                  }
+                              } else {
+                                  if (features[0] < 18502) {
+                                      if (features[0] < 17752) {
+                                          return 2;
+                                      } else {
+                                          return 1;
+                                      }
+                                  } else {
+                                      return 2;
+                                  }
+                              }
+                          }
+                      } else {
+                          if (features[2] < 31204) {
+                              if (features[0] < 31252) {
+                                  if (features[2] < 117) {
+                                      if (features[0] < 13252) {
+                                          return 1;
+                                      } else {
+                                          return 2;
+                                      }
+                                  } else {
+                                      if (features[1] < 12263) {
+                                          return 2;
+                                      } else {
+                                          return 1;
+                                      }
+                                  }
+                              } else {
+                                  if (features[3] < 614178) {
+                                      return 2;
+                                  } else {
+                                      if (features[1] < 11665) {
+                                          return 1;
+                                      } else {
+                                          return 2;
+                                      }
+                                  }
+                              }
+                          } else {
+                              if (features[3] < 1260893) {
+                                  if (features[1] < 11461) {
+                                      if (features[1] < 10456) {
+                                          return 1;
+                                      } else {
+                                          return 2;
+                                      }
+                                  } else {
+                                      return 1;
+                                  }
+                              } else {
+                                  return 2;
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+        }
+        
+
+int32_t io_dt_predict(const int16_t *features, int32_t features_length) {
+
+        int32_t votes[3] = {0,};
+        int32_t _class = -1;
+
+        _class = io_dt_tree_0(features, features_length); votes[_class] += 1;
+    
+        int32_t most_voted_class = -1;
+        int32_t most_voted_votes = 0;
+        for (int32_t i=0; i<3; i++) {
+
+            if (votes[i] > most_voted_votes) {
+                most_voted_class = i;
+                most_voted_votes = votes[i];
+            }
+        }
+        return most_voted_class;
+    }
+    
+
+int io_dt_predict_proba(const int16_t *features, int32_t features_length, float *out, int out_length) {
+
+        int32_t _class = -1;
+
+        for (int i=0; i<out_length; i++) {
+            out[i] = 0.0f;
+        }
+
+        _class = io_dt_tree_0(features, features_length); out[_class] += 1.0f;
+    
+        // compute mean
+        for (int i=0; i<out_length; i++) {
+            out[i] = out[i] / 1;
+        }
+        return 0;
+    }
+    
